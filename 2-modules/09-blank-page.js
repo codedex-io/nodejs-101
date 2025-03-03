62819938cd04bea4f9b50f62f552d98094adba91
Julien Kris <hi.jules.kris@gmail.com>
2025-03-03 15:15:01 -0500

    Update 09-blank-page.js

--- 2-modules/09-blank-page.js
+++ 2-modules/09-blank-page.js
@@ -12,3 +12,29 @@ fs.unlink(filePath, (err) => {
   }
   console.log("Diary entry deleted!!");
 });
+
+/* 
+//Solution for Bonus Exercise
+//You can apply this to the original file before deleting it
+
+const fs = require("fs");
+// Read the file
+fs.readFile("seventh-grade.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+
+  // Replace all Latin letters with rectangles
+  const redactedText = data.replace(/[A-Za-z]/g, "█");
+  // Write the modified content back to the file
+  fs.writeFile("seventh-grade.txt", redactedText, (err) => {
+    if (err) {
+      console.error("Error redacting file:", err);
+      return;
+    }
+    console.log("\nKEEP OUT!!!!!!\n", data);
+  });
+});
+
+*/
