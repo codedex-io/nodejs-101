c4297949f7eb42e9648ffe26a9765168078a84d1
Julien Kris <hi.jules.kris@gmail.com>
2025-05-02 14:46:33 -0400

    update 19 solution code

--- 4-http-methods/19-oh-crud.js
+++ 4-http-methods/19-oh-crud.js
@@ -6,7 +6,7 @@ const http = require('http'); // Import the http module
 // Create a server
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  console.log('Discord mapped to CRUD:');
+  console.log('App Name: Discord');
   console.log('Create: Write a message in a channel');
   console.log('Read: See who has reacted to my message');
   console.log('Update: Edit my message to fix a typo');
